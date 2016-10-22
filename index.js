b82948f49fe4092a2903e05b8115d0fbfcd3ecbb
roxannekim <rk3082@nyu.edu>
2016-10-22 17:20:26 -0400

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 var express = require('express')
+var path = require('path')
 
 var app = express();
 
 var http = require('http')
 http = http.Server(app)
 
+//MIDDLEWARE
+//external library
+app.set('views','path.join(__dirname,'views'))
+app.set('view engine','pug')
+//set and just use
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.get('/',function(req, res){
+
+	var name = "Roxanne"
+
+	// get name from database
+
+	//add first name and last name
+
+	//set it to a variable
+
+	//pass that variable to the template
 
 	// [browser] request  <->  response [server]
 
-	res.send("hello ITP")
+	res.render("index.pug", {myname:name})
+	
+	//res.send("<h1>hello ITP<h1>")
 
 })
 
 
 app.get('/about, function(req, res'){
 
-	res.send("ABOUT ITP")
+	res.render("about.pug")
+	//res.send("ABOUT ITP")
 
 }
 
